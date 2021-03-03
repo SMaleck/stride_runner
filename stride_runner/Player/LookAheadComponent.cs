8cd57afd043673ce2b2e2ea9b559629e283ef250
Sebastian Maleck <sebastian.maleck@kolibrigames.com>
2021-03-03 19:13:00 +0100

    Adds Spawner boilerplate

---
+++ stride_runner/Player/LookAheadComponent.cs
@@ -0,0 +1,18 @@
+﻿using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Input;
+using Stride.Physics;
+
+namespace Source.Player
+{
+    public class LookAheadComponent : SyncScript
+    {
+        public override void Start()
+        {
+        }
+
+        public override void Update()
+        {
+        }
+    }
+}
---
+++ stride_runner/World/BackgroundSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Source.Player;
+using Stride.Core.Serialization;
+using Stride.Engine;
+using Stride.Rendering;
+
+namespace Source.World
+{
+    public class BackgroundSpawner : SyncScript
+    {
+        public LookAheadComponent LookAheadComponent;
+        public Entity Cube;
+
+        public override void Start()
+        {
+            // ToDo Get Lookahead Component
+        }
+
+        public override void Update()
+        {
+            // ToDo Spawn blocks ahead of player
+            // ToDo reuse Spawned blocks when out of sight
+        }
+
+        private void CreateCube()
+        {
+            var clone = Cube.Clone();
+        }
+    }
+}
